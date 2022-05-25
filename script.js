8d1c7fefaf6b4ab915d22a19dca837233e7c9aa6
Denver12345 <villaan12345>
2022-05-25 14:56:21 +0300

    fixed bags

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-const firstNumb = Number(prompt('Enter first number'));
+const firstNumb = prompt('Enter first number');
 const operation = prompt("Enter operator");
-const secondNumb = Number(prompt('Enter second number'));
+const secondNumb = prompt('Enter second number');
 
-const isNumb1 = isNaN(firstNumb)
-const isNumb2 = isNaN(secondNumb)
+console.log(firstNumb)
+console.log(secondNumb)
+
+const isNumb1 = isNaN(Number(firstNumb))
+const isNumb2 = isNaN(Number(secondNumb))
+console.log(isNumb1)
+console.log(isNumb2)
 
 if(isNumb1 === false && isNumb2 === false) {
 
     if (firstNumb !== null && operation !== null && secondNumb !== null) {
         if (operation === '+') {
-            alert(`Ваш результат ${firstNumb + secondNumb}`)
+            alert(`Ваш результат ${ Number(firstNumb) + Number(secondNumb)}`)
         } else if (operation === '-') {
-            alert(`Ваш результат ${firstNumb - secondNumb}`)
+            alert(`Ваш результат ${Number(firstNumb) - Number(secondNumb)}`)
         } else if (operation === '*') {
-            alert(`Ваш результат ${firstNumb * secondNumb}`)
+            alert(`Ваш результат ${Number(firstNumb) * Number(secondNumb)}`)
         } else if (operation === '/') {
-            alert(`Ваш результат ${firstNumb / secondNumb}`)
+            alert(`Ваш результат ${Number(firstNumb) / Number(secondNumb)}`)
         } else {
             alert("You entered an invalid operator")
         }
